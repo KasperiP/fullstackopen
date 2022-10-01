b98fe9636bf3b63697bbe4376400bc09a3bb4316
kassq <kasperi.pohtine@gmail.com>
2022-10-01 20:19:33 +0300

    Start osa4

---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,78 @@
+const dummy = (blogs) => {
+	return 1;
+};
+
+const totalLikes = (blogs) => {
+	return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+	const likes = blogs.map((blog) => blog.likes);
+	const max = Math.max(...likes);
+	const favorite = blogs.find((blog) => blog.likes === max);
+	return favorite || null;
+};
+
+const mostBlogs = (blogs) => {
+	const authors = blogs.map((blog) => blog.author);
+	const uniqueAuthors = [...new Set(authors)];
+
+	let mostBlogsAuthor;
+	uniqueAuthors.forEach((name) => {
+		const authorBlogs = authors.filter(
+			(authorName) => authorName === name
+		).length;
+
+		if (!mostBlogsAuthor) {
+			mostBlogsAuthor = {
+				author: name,
+				blogs: authorBlogs,
+			};
+		} else {
+			if (mostBlogsAuthor.blogs < authorBlogs) {
+				mostBlogsAuthor = {
+					author: name,
+					blogs: authorBlogs,
+				};
+			}
+		}
+	});
+
+	return mostBlogsAuthor || null;
+};
+
+const mostLikes = (blogs) => {
+	const authors = blogs.map((blog) => blog.author);
+	const uniqueAuthors = [...new Set(authors)];
+
+	let mostLikedAuthor;
+	uniqueAuthors.forEach((authorName) => {
+		const likes = blogs
+			.filter((blog) => blog.author == authorName)
+			.reduce((sum, blog) => sum + blog.likes, 0);
+
+		if (!mostLikedAuthor) {
+			mostLikedAuthor = {
+				author: authorName,
+				likes,
+			};
+		} else {
+			if (mostLikedAuthor.likes < likes) {
+				mostLikedAuthor = {
+					author: authorName,
+					likes,
+				};
+			}
+		}
+	});
+
+	return mostLikedAuthor || null;
+};
+
+module.exports = {
+	dummy,
+	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+	mostLikes,
+};
