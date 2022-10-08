6beafe68d7c43c43eb10aae87225f39b661d9f5f
kassq <kasperi.pohtine@gmail.com>
2022-10-09 01:18:17 +0300

    Handle TokenExpiredError

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -23,6 +23,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'invalid token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' })
   }
 
   logger.error(error?.message)
