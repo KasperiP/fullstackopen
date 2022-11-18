f55f7b2028998ae85121747efc65693426564995
kassq <kasperi.pohtine@gmail.com>
2022-11-18 14:08:37 +0200

    Finish exercises 9.1-9.3

---
+++ osa9/src/bmiCalculator.ts
@@ -0,0 +1,44 @@
+const calculateBmi = (height: number, weight: number): string => {
+	const values = {
+		severe_thinness: 16,
+		moderate_thinness: 16.9,
+		mild_thinness: 18.4,
+		normal: 24.9,
+		overweight: 29.9,
+		obese_class_1: 34.9,
+		obese_class_2: 39.9,
+		obese_class_3: 40,
+	};
+
+	const bmi = weight / (height / 100) ** 2;
+
+	if (bmi < values.severe_thinness) {
+		return 'Underweight (Severe thinness)';
+	}
+	if (bmi < values.moderate_thinness) {
+		return 'Underweight (Moderate thinness)';
+	}
+	if (bmi < values.mild_thinness) {
+		return 'Underweight (Mild thinness)';
+	}
+	if (bmi < values.normal) {
+		return 'Normal (Healthy weight)';
+	}
+	if (bmi < values.overweight) {
+		return 'Overweight (Pre-obese)';
+	}
+	if (bmi < values.obese_class_1) {
+		return 'Obese (Class I)';
+	}
+	if (bmi < values.obese_class_2) {
+		return 'Obese (Class II)';
+	}
+	if (bmi < values.obese_class_3) {
+		return 'Obese (Class III)';
+	}
+};
+
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+
+console.log(calculateBmi(height, weight));
---
+++ osa9/src/exerciseCalculator.ts
@@ -0,0 +1,46 @@
+interface Result {
+	periodLength: number;
+	trainingDays: number;
+	success: boolean;
+	rating: number;
+	ratingDescription: string;
+	target: number;
+	average: number;
+}
+
+const calculateExercises = (exercises: number[], target: number): Result => {
+	const periodLength = exercises.length;
+	const trainingDays = exercises.filter((exercise) => exercise > 0).length;
+	const average = exercises.reduce((a, b) => a + b, 0) / periodLength;
+	const success = average >= target;
+
+	let rating = 0;
+	let ratingDescription = '';
+	if (average < target) {
+		rating = 1;
+		ratingDescription = 'work harder';
+	}
+	if (average >= target) {
+		rating = 2;
+		ratingDescription = 'not too bad but could be better';
+	}
+	if (average >= target * 1.5) {
+		rating = 3;
+		ratingDescription = 'you are doing great';
+	}
+
+	return {
+		periodLength,
+		trainingDays,
+		success,
+		rating,
+		ratingDescription,
+		target,
+		average,
+	};
+};
+
+const exercises = process.argv.slice(3).map((exercise) => Number(exercise));
+const target = Number(process.argv[2]);
+
+console.log(calculateExercises(exercises, target));
