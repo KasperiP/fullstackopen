68496c7989d2316e48a4b0e711541892a09d4550
kassq <kasperi.pohtine@gmail.com>
2022-11-18 15:30:37 +0200

    Finish exercises 9.8-9.9

---
+++ osa9/patientor/index.ts
@@ -0,0 +1,19 @@
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+// Cors for cross-origin allowance
+app.use(cors());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+	console.log('someone pinged here');
+	res.send('pong');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
